ddc43f12721bfa644ccc92b744c53095cc155741
dedoxd2 <dedo.xd33@yahoo.com>
2023-10-23 22:30:59 +0300

    taking screenshots

--- tests/test_ex6.py
+++ tests/test_ex6.py
@@ -56,8 +56,8 @@ from selenium import webdriver
 #     web_driver.close
 
 
-@pytest.mark.usefixtures("driver_init")
-class Test_URL_Chrome:
-    def test_open_url(self, live_server):
-        self.driver.get(("%s%s" % (live_server.url, "/admin/")))
-        assert "Log in | Django site admin" in self.driver.title
+# @pytest.mark.usefixtures("driver_init")
+# class Test_URL_Chrome:
+#     def test_open_url(self, live_server):
+#         self.driver.get(("%s%s" % (live_server.url, "/admin/")))
+#         assert "Log in | Django site admin" in self.driver.title
---
+++ tests/test_ex7.py
@@ -0,0 +1,62 @@
+import os
+import time
+import pytest
+from django.test import LiveServerTestCase
+from selenium import webdriver
+
+
+def take_screenshot(driver, name):
+    time.sleep(1)
+    os.makedirs(os.path.join(
+        "screenshot", os.path.dirname(name)), exist_ok=True)
+    driver.save_screenshot(os.path.join("screenshot", name))
+
+# Example 1
+# def test_example(live_server):
+#     driver = webdriver.Chrome()
+#     driver.get(("%s%s" % (live_server.url, "/admin/")))
+#     take_screenshot(driver, "admin/admin.png")
+
+# Example 2
+# def test_open_url(live_server):
+#     options = webdriver.ChromeOptions()
+#     options.add_argument("--healdess")
+#     options.add_argument("--window-size=1920,1080")
+#     chrome_driver = webdriver.Chrome(options=options)
+#     chrome_driver.get(("%s%s" % (live_server.url, "/admin/")))
+#     take_screenshot(chrome_driver, "admin/admin.png")
+
+
+@pytest.fixture(params=["chrome1920", "chrome411", "firefox"], scope="class")
+def driver_init(request):
+    if request.param == "chrome1920":
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--window-size=1920,1080")
+        web_driver = webdriver.Chrome(options=options)
+        request.cls.browser = "Chrome1920x1080"
+    if request.param == "chrome411":
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--window-size=411,823")
+        web_driver = webdriver.Chrome(options=options)
+        request.cls.browser = "Chrome411x823"
+
+    if request.param == "firefox":
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--headless")
+        options.add_argument("--window-size=411,823")
+        web_driver = webdriver.Firefox(options=options)
+        request.cls.browser = "Firefox"
+
+    request.cls.driver = web_driver
+    yield
+    web_driver.close()
+
+
+@pytest.mark.usefixtures("driver_init")
+class Screenshot:
+    def screenshot_admin(self, live_server):
+        self.driver.get(("%s%s" % (live_server.url, "/admin/")))
+        take_screenshot(self.driver, "admin/"+"admin"+self.browser + ".png")
+        assert "Log in | Django site admin" in self.driver.title
